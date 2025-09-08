37c3708ef744d07c11a7310ee4e7dd1fece5dc6a
Veniamin <ilkovveniamin@gmail.com>
2025-09-08 09:58:15 +0500

    build: Add Bank_RESTApplication class and update application.yml for environment configuration

---
+++ src/main/java/com/example/bankcards/Bank_RESTApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bank_RESTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bank_RESTApplication.class, args);
+    }
+
+}
