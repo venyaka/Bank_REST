065ecede7ae43d5ff5aa35f6b7c67a468889fcfc
Veniamin <ilkovveniamin@gmail.com>
2025-09-08 17:18:33 +0500

    feat: Add User, Role, and UserSession entities with JPA annotations and security integration

---
+++ src/main/java/com/example/bankcards/entity/Role.java
@@ -0,0 +1,13 @@
+package com.example.bankcards.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+
+    USER, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return this.name();
+    }
+}
