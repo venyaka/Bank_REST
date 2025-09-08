11dc5bbaf2d6be64a5143b7bba6a2fd58d831c0a
Veniamin <ilkovveniamin@gmail.com>
2025-09-08 17:21:05 +0500

    feat: Add SecurityBeans and SecurityConfig for JWT authentication and CORS configuration

---
+++ src/main/java/com/example/bankcards/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.example.bankcards.config;
+
+import com.example.bankcards.security.RefreshTokenFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import com.example.bankcards.security.ExceptionHandlerFilter;
+import com.example.bankcards.security.jwt.JwtTokenFilter;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenFilter jwtTokenFilter;
+    private final RefreshTokenFilter refreshTokenFilter;
+
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PATCH", "DELETE", "PUT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        configuration.applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(exceptionHandlerFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(jwtTokenFilter, ExceptionHandlerFilter.class)
+//                .addFilterAfter(refreshTokenFilter, RefreshTokenFilter.class)
+                .exceptionHandling(Customizer.withDefaults())
+                .authorizeHttpRequests(c ->
+                        c
+                                .requestMatchers("/favicon.ico", "/css/**", "/js/**", "/images/**").permitAll()
+                                .requestMatchers("/swagger/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+
+                                .requestMatchers("/authorize/**").permitAll()
+
+                                .requestMatchers("/users/**").authenticated()
+
+                                .requestMatchers("/profile/**").authenticated()
+                                .requestMatchers("/admin/**").hasAuthority("ADMIN")
+
+                                .anyRequest().denyAll()
+                )
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .logout(c -> c.invalidateHttpSession(true).clearAuthentication(true))
+                .sessionManagement(c -> c.maximumSessions(1))
+                .build();
+    }
+}
