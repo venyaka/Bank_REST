7e0ebebce9fa21ab864c740e099f882eedf8cd0b
Veniamin <ilkovveniamin@gmail.com>
2025-09-08 17:22:47 +0500

    feat: Add constants for IP addresses and API paths

---
+++ src/main/java/com/example/bankcards/constant/PathConstants.java
@@ -0,0 +1,7 @@
+package com.example.bankcards.constant;
+
+public class PathConstants {
+    public static final String AUTHORIZE_CONTROLLER_PATH = "/authorize";
+    public static final String USER_CONTROLLER_PATH = "/users";
+
+}
