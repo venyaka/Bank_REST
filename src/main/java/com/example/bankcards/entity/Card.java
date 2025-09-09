5c350773fcc0618f170c101bcc24ba9f9c8cf509
Veniamin <ilkovveniamin@gmail.com>
2025-09-09 14:49:31 +0500

    feat: Add Card entity and CardStatus enum for bank card management

---
+++ src/main/java/com/example/bankcards/entity/Card.java
@@ -0,0 +1,39 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "card")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "card_number", nullable = false)
+    private String cardNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User owner;
+
+    @Column(name = "expire_date", nullable = false)
+    private LocalDate expireDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private CardStatus status;
+
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+}
+
