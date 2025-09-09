ad07297449d3dfff873b118cb0549e72ad7184ad
Veniamin <ilkovveniamin@gmail.com>
2025-09-09 14:55:57 +0500

    feat: Add CardBlockRequest entity for managing card block requests

---
+++ src/main/java/com/example/bankcards/entity/CardBlockRequest.java
@@ -0,0 +1,49 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "card_block_request")
+@Getter
+@Setter
+@NoArgsConstructor
+public class CardBlockRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id")
+    private Card card;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "admin_id")
+    private User admin;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "processed_at")
+    private LocalDateTime processedAt;
+
+    @Column(name = "admin_comment")
+    private String adminComment;
+
+    public enum Status {
+        PENDING, APPROVED, REJECTED
+    }
+}
