86cdb42c6d9f2276df734261603b81e655687dd6
Veniamin <ilkovveniamin@gmail.com>
2025-09-09 14:45:08 +0500

    feat: Extend UserService interface with user management methods

--- src/main/java/com/example/bankcards/service/UserService.java
+++ src/main/java/com/example/bankcards/service/UserService.java
@@ -1,9 +1,13 @@
 package com.example.bankcards.service;
 
+import com.example.bankcards.dto.request.CreateUserReqDTO;
+import com.example.bankcards.dto.request.UpdateUserReqDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import com.example.bankcards.dto.request.UpdateCurrentUserReqDTO;
 import com.example.bankcards.dto.response.UserRespDTO;
 import com.example.bankcards.entity.User;
+
+import java.util.List;
 
 public interface UserService extends UserDetailsService {
 
@@ -17,4 +21,13 @@ public interface UserService extends UserDetailsService {
 
     UserRespDTO getResponseDTO(User user);
 
-    void logout();}
+    void logout();
+
+    List<UserRespDTO> getAllUsers();
+
+    UserRespDTO createUser(CreateUserReqDTO createUserReqDTO);
+
+    UserRespDTO updateUser(Long id, UpdateUserReqDTO updateUserReqDTO);
+
+    void deleteUser(Long id);
+}
