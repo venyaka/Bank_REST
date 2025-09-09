69518950d609c18b2e461363c999d632347f3fdc
Veniamin <ilkovveniamin@gmail.com>
2025-09-09 14:57:31 +0500

    feat: Add new path constants for card and admin controllers

--- src/main/java/com/example/bankcards/constant/PathConstants.java
+++ src/main/java/com/example/bankcards/constant/PathConstants.java
@@ -3,5 +3,9 @@ package com.example.bankcards.constant;
 public class PathConstants {
     public static final String AUTHORIZE_CONTROLLER_PATH = "/authorize";
     public static final String USER_CONTROLLER_PATH = "/users";
-
+    public static final String CARD_CONTROLLER_PATH = "/cards";
+    public static final String ADMIN_CONTROLLER_PATH = "/admin";
+    public static final String BLOCK_REQUEST_CONTROLLER_PATH = "/admin/cards/block-requests";
+    public static final String CARD_ADMIN_CONTROLLER_PATH = "/admin/cards";
+    public static final String USER_ADMIN_CONTROLLER_PATH = "/admin/users";
 }
