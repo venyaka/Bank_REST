b6e94eef9d3de897a3b087f0a8a85522b2cc8533
Veniamin <ilkovveniamin@gmail.com>
2025-09-09 14:57:18 +0500

    feat: Add additional error messages for card operations and not found scenarios

--- src/main/java/com/example/bankcards/exception/errors/NotFoundError.java
+++ src/main/java/com/example/bankcards/exception/errors/NotFoundError.java
@@ -2,6 +2,7 @@ package com.example.bankcards.exception.errors;
 
 public enum NotFoundError {
 
+    CARD_NOT_FOUND("Карта не найдена"),
     USER_NOT_FOUND("Пользователь не был найден");
 
     private String message;
